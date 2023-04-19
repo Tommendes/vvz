68ef1af8fe2defc3e30fff8ba5b0e18181f410ad
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-04-19 18:54:34 -0300

    Criação da migration ged_protolo

---
+++ app/migrations/20230419214744_create_table_ged_protolo.js
@@ -0,0 +1,22 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('vivazul_cso_jp.ged_protolo', table => {
+        table.engine('InnoDB')
+        table.charset('utf8mb4')
+        table.collate('utf8mb4_general_ci')
+        table.increments('id').primary()
+        table.integer('evento').notNull()
+        table.string('created_at').notNull()
+        table.string('updated_at')
+        table.string('status').default(0).notNull().comment('Status do registro (INATIVO:0; ATIVO:10; EXCLUÍDO:99)')
+        table.foreign('id_cadastros').references('id').inTable('cadastros').onUpdate('Cascade').onDelete('NO ACTION')
+        table.string('email_destinatario',255).comment('Destinatário(email)')
+        table.string('registro',255).comment('Número do protocolo')
+        table.string('titulo',255).comment('Port')
+        table.string('e_s',255).comment('Movimento')
+        table.string('descricao',255).comment('Descrição do documento')
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('vivazul_cso_jp.ged_protolo')
+};
