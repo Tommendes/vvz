f997b0f66b81e844e271538236b8c55a892ee4ff
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-04-25 15:47:39 -0300

    Criação de id_cadastros

--- app/migrations/20230419214744_create_table_ged_protolo.js
+++ app/migrations/20230419214744_create_table_ged_protolo.js
@@ -8,12 +8,13 @@ exports.up = function(knex, Promise) {
         table.string('created_at').notNull()
         table.string('updated_at')
         table.string('status').default(0).notNull().comment('Status do registro (INATIVO:0; ATIVO:10; EXCLUÍDO:99)')
-        table.foreign('id_cadastros').references('id').inTable('cadastros').onUpdate('Cascade').onDelete('NO ACTION')
+        table.integer('id_cadastros',10).unsigned().notNull().comment('Registro do Destinatário no cadastro')
         table.string('email_destinatario',255).comment('Destinatário(email)')
         table.string('registro',255).comment('Número do protocolo')
         table.string('titulo',255).comment('Port')
         table.string('e_s',255).comment('Movimento')
         table.string('descricao',255).comment('Descrição do documento')
+        table.foreign('id_cadastros').references('id').inTable('cadastros').onUpdate('Cascade').onDelete('NO ACTION')
     })
 };
 
