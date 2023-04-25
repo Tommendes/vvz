566922b61276d65aec4c2c90a957066c81259186
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-04-25 16:06:37 -0300

    Adicionando id_ged

--- app/migrations/20230419215458_create_table_com_terceiros.js
+++ app/migrations/20230419215458_create_table_com_terceiros.js
@@ -8,12 +8,13 @@ exports.up = function(knex, Promise) {
         table.string('created_at').notNull()
         table.string('updated_at')
         table.string('status').default(0).notNull().comment('Status do registro (INATIVO:0; ATIVO:10; EXCLUÍDO:99)')
-        table.foreign('id_ged').references('id').inTable('ged').onUpdate('Cascade').onDelete('NO ACTION')
+        table.integer('id_ged',11).notNull().comment('Documento relacionado ')
         table.foreign('id_com_agentes').references('id').inTable('com_agentes').onUpdate('Cascade').onDelete('NO ACTION')
         table.boolean('terceiro').comment('Se um terceiro')
         table.decimal('valor_base',10,2).comment('Valor base de cálculo da comissão')
         table.decimal('participacao',10,2).comment('Percentual de comissão')
         table.string('liquidacao',255).comment('Data da liquidação')
+        table.foreign('id_ged').references('id').inTable('ged').onUpdate('Cascade').onDelete('NO ACTION')
     })
 };
 
