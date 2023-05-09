f12d68d3821ba3cb036223c003387ab658d0ec35
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-05-09 15:24:40 -0300

    Criação da migartion pv_tecnicos

---
+++ app/migrations/20230509181247_create_table_pv_tecnicos.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('vivazul_cso_jp.pv_tecnicos', table => {
+        table.engine('InnoDB')
+        table.charset('utf8mb4')
+        table.collate('utf8mb4_general_ci')
+        table.increments('id').primary()
+        table.integer('evento').notNull()
+        table.string('created_at').notNull()
+        table.string('updated_at')
+        table.string('status').default(0).notNull().comment('Status do registro (INATIVO:0; ATIVO:10; EXCLUÍDO:99)')
+        table.string('tecnico',255).comment('Técnico responsável')
+        table.string('telefone_contato',255).notNull().comment('Telefone do contato')
+        table.string('email_contato',255).comment('Email do contato')
+        table.text('observacao').comment('Descrição dos serviços')
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('vivazul_cso_jp.pv_tecnicos')
+};
