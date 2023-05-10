d91010c2b89fe7fec79a6f2558f16aed1cf021bf
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-05-10 16:47:20 -0300

    Criação de pv_tecnicos.js

---
+++ app/api/pv_tecnicos.js
@@ -0,0 +1,197 @@
+const { dbPrefix } = require("../.env")
+module.exports = app => {
+    const { existsOrError, notExistsOrError, cpfOrError, cnpjOrError, lengthOrError, emailOrError, isMatchOrError, noAccessMsg } = app.api.validation
+    const tabela = 'pv_tecnicos'
+    const STATUS_ACTIVE = 10
+    const STATUS_DELETE = 99
+
+    const save = async (req, res) => {
+        let user = req.user
+        const uParams = await app.db('users').where({ id: user.id }).first();
+        let body = { ...req.body }
+        if (req.params.id) body.id = req.params.id
+        try {
+            // Alçada para edição
+            if (body.id)
+                isMatchOrError(uParams, `${noAccessMsg} "Edição de ${tabela}"`)
+            // Alçada para inclusão
+            else isMatchOrError(uParams, `${noAccessMsg} "Inclusão de ${tabela}"`)
+        } catch (error) {
+            return res.status(401).send(error)
+        }
+        const tabelaDomain = `${dbPrefix}_${user.cliente}_${user.dominio}.${tabela}`
+
+        try {
+            existsOrError(body.telefone_contato, 'Telefone de contato não encontrado')
+
+        } catch (error) {
+            return res.status(400).send(error)
+        }
+
+        delete body.hash; delete body.tblName
+        if (body.id) {
+            // Variáveis da edição de um registro
+            // registrar o evento na tabela de eventos
+            const { createEventUpd } = app.api.sisEvents
+            const evento = await createEventUpd({
+                "notTo": ['created_at', 'updated_at', 'evento',],
+                "last": await app.db(tabelaDomain).where({ id: body.id }).first(),
+                "next": body,
+                "request": req,
+                "evento": {
+                    "evento": `Alteração de cadastro de ${tabela}`,
+                    "tabela_bd": tabela,
+                }
+            })
+
+            body.evento = evento
+            body.updated_at = new Date()
+            let rowsUpdated = app.db(tabelaDomain)
+                .update(body)
+                .where({ id: body.id })
+            rowsUpdated.then((ret) => {
+                if (ret > 0) res.status(200).send(body)
+                else res.status(200).send('Endereço não encontrado')
+            })
+                .catch(error => {
+                    app.api.logger.logError({ log: { line: `Error in file: ${__filename} (${__function}). Error: ${error}`, sConsole: true } })
+                    return res.status(500).send(error)
+                })
+        } else {
+            // Criação de um novo registro
+            const nextEventID = await app.db('sis_events').select(app.db.raw('count(*) as count')).first()
+
+            body.evento = nextEventID.count + 1
+            // Variáveis da criação de um novo registro
+            body.status = STATUS_ACTIVE
+            body.created_at = new Date()
+
+            app.db(tabelaDomain)
+                .insert(body)
+                .then(ret => {
+                    body.id = ret[0]
+                    // registrar o evento na tabela de eventos
+                    const { createEventIns } = app.api.sisEvents
+                    createEventIns({
+                        "notTo": ['created_at', 'evento'],
+                        "next": body,
+                        "request": req,
+                        "evento": {
+                            "evento": `Novo registro`,
+                            "tabela_bd": tabela,
+                        }
+                    })
+                    return res.json(body)
+                })
+                .catch(error => {
+                    app.api.logger.logError({ log: { line: `Error in file: ${__filename} (${__function}). Error: ${error}`, sConsole: true } })
+                    return res.status(500).send(error)
+                })
+        }
+    }
+
+    const limit = 20 // usado para paginação
+    const get = async (req, res) => {
+        let user = req.user
+        let key = req.query.key
+        if (key) {
+            key = key.trim()
+        }
+        const uParams = await app.db('users').where({ id: user.id }).first();
+        try {
+            // Alçada para exibição
+            isMatchOrError(uParams, `${noAccessMsg} "Exibição de cadastro de ${tabela}"`)
+        } catch (error) {
+            return res.status(401).send(error)
+        }
+
+        const tabelaDomain = `${dbPrefix}_${uParams.cliente}_${uParams.dominio}.${tabela}`
+        const page = req.query.page || 1
+        let count = app.db({ tbl1: tabelaDomain }).count('* as count')
+            .where({ status: STATUS_ACTIVE })
+        count = await app.db.raw(count.toString())
+        count = count[0][0].count
+
+        const ret = app.db({ tbl1: tabelaDomain })
+            .select(app.db.raw(`tbl1.*, SUBSTRING(SHA(CONCAT(id,'${tabela}')),8,6) as hash`))
+
+        ret.where({ status: STATUS_ACTIVE })
+            .groupBy('tbl1.id')
+            .limit(limit).offset(page * limit - limit)
+            .then(body => {
+                return res.json({ data: body, count: count, limit })
+            })
+            .catch(error => {
+                app.api.logger.logError({ log: { line: `Error in file: ${__filename} (${__function}). Error: ${error}`, sConsole: true } })
+            })
+    }
+
+
+    const getById = async (req, res) => {
+        let user = req.user
+        const uParams = await app.db('users').where({ id: user.id }).first();
+        try {
+            // Alçada para exibição
+            isMatchOrError(uParams, `${noAccessMsg} "Exibição de Endereços de ${tabela}"`)
+        } catch (error) {
+            return res.status(401).send(error)
+        }
+
+        const tabelaDomain = `${dbPrefix}_${uParams.cliente}_${uParams.dominio}.${tabela}`
+        const ret = app.db({ tbl1: tabelaDomain })
+            .select(app.db.raw(`tbl1.*, TO_BASE64('${tabela}') tblName, SUBSTRING(SHA(CONCAT(tbl1.id,'${tabela}')),8,6) as hash`))
+            .where({ 'tbl1.id': req.params.id, 'tbl1.status': STATUS_ACTIVE }).first()
+            .then(body => {
+                return res.json(body)
+            })
+            .catch(error => {
+                app.api.logger.logError({ log: { line: `Error in file: ${__filename} (${__function}). Error: ${error}`, sConsole: true } })
+                return res.status(500).send(error)
+            })
+    }
+
+    const remove = async (req, res) => {
+        let user = req.user
+        const uParams = await app.db('users').where({ id: user.id }).first();
+        try {
+            // Alçada para exibição
+            isMatchOrError((uParams && uParams.admin >= 1), `${noAccessMsg} "Exclusão de Endereço de ${tabela}"`)
+        } catch (error) {
+            return res.status(401).send(error)
+        }
+
+        const tabelaDomain = `${dbPrefix}_${uParams.cliente}_${uParams.dominio}.${tabela}`
+        const registro = { status: STATUS_DELETE }
+        try {
+            // registrar o evento na tabela de eventos
+            const last = await app.db(tabelaDomain).where({ id: req.params.id }).first()
+            const { createEventUpd } = app.api.sisEvents
+            const evento = await createEventUpd({
+                "notTo": ['created_at', 'updated_at', 'evento'],
+                "last": last,
+                "next": registro,
+                "request": req,
+                "evento": {
+                    "classevento": "Remove",
+                    "evento": `Exclusão de Endereço de ${tabela}`,
+                    "tabela_bd": tabela,
+                }
+            })
+            const rowsUpdated = await app.db(tabelaDomain)
+                .update({
+                    status: registro.status,
+                    updated_at: new Date(),
+                    evento: evento
+                })
+                .where({ id: req.params.id })
+            existsOrError(rowsUpdated, 'Registro não foi encontrado')
+
+            res.status(204).send()
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    }
+
+
+    return { save, get, getById, remove }
+}
