55de9bd3794177cd3a204c5dde2f486eb61b9ea2
Gabriel Gomes <gabrielgomesport@gmail.com>
2023-10-23 17:42:45 -0300

    Dashboard
    Criação dos Grids e Forms: Pv e Tecnicos

---
+++ dashboard/src/views/pos-vendas/PosVendaForm.vue
@@ -0,0 +1,210 @@
+<script setup>
+import { onBeforeMount, onMounted, ref, watch, watchEffect } from 'vue';
+import { baseApiUrl } from '@/env';
+import axios from '@/axios-interceptor';
+import { defaultSuccess, defaultWarn } from '@/toast';
+import { isValidEmail } from '@/global';
+import { userKey } from '@/global';
+const json = localStorage.getItem(userKey);
+const userData = JSON.parse(json);
+
+import Breadcrumb from '@/components/Breadcrumb.vue';
+
+import { Mask } from 'maska';
+const masks = ref({
+    telefone: new Mask({
+        mask: ['(##) ####-####', '(##) #####-####']
+    })
+});
+
+import { useRoute } from 'vue-router';
+const route = useRoute();
+
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+// Cookies de usuário
+import { useUserStore } from '@/stores/user';
+const store = useUserStore();
+
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
+
+// Campos de formulário
+const itemData = ref({});
+const registroTipo = ref('pf');
+const labels = ref({
+    id_cadastros: "Técnico Responsável",
+    id_pipeline: "Telefone para Contato",
+    tipo: "E-mail para contato",
+    pv_nr: "Observação",
+    observacao: "Técnico Responsável"
+});
+// Modelo de dados usado para comparação
+const itemDataComparision = ref({});
+// Modo do formulário
+const mode = ref('view');
+// Aceite do formulário
+const accept = ref(false);
+// Mensages de erro
+const errorMessages = ref({});
+// Loadings
+const loading = ref({
+    form: true,
+    accepted: null,
+    email: null,
+    telefone: null
+});
+// Props do template
+const props = defineProps({
+    mode: String
+});
+// Emit do template
+const emit = defineEmits(['changed', 'cancel']);
+// Url base do form action
+const urlBase = ref(`${baseApiUrl}/pv-tecnicos`);
+// Carragamento de dados do form
+const loadData = async () => {
+    if (route.params.id || itemData.value.id) {
+        if (route.params.id) itemData.value.id = route.params.id;
+        const url = `${urlBase.value}/${itemData.value.id}`;
+        // console.log('loadData',url);
+        await axios.get(url).then((res) => {
+            const body = res.data;
+            if (body && body.id) {
+                body.id = String(body.id);
+
+                itemData.value = body;
+                if (itemData.value.telefone_contato) itemData.value.telefone_contato = masks.value.telefone.masked(itemData.value.telefone_contato);
+                itemDataComparision.value = { ...itemData.value };
+
+                loading.value.form = false;
+            } else {
+                defaultWarn('Registro não localizado');
+                router.push({ path: `/${store.userStore.cliente}/${store.userStore.dominio}/pv-tecnicos` });
+            }
+        });
+    } else loading.value.form = false;
+};
+// Salvar dados do formulário
+const saveData = async () => {
+    if (formIsValid()) {
+        const method = itemData.value.id ? 'put' : 'post';
+        const id = itemData.value.id ? `/${itemData.value.id}` : '';
+        const url = `${urlBase.value}${id}`;
+        if (itemData.value.telefone_contato) itemData.value.telefone_contato = masks.value.telefone.unmasked(itemData.value.telefone_contato);
+        axios[method](url, itemData.value)
+            .then((res) => {
+                const body = res.data;
+                if (body && body.id) {
+                    defaultSuccess('Registro salvo com sucesso');
+                    itemData.value = body;
+                    itemDataComparision.value = { ...itemData.value };
+                    if (mode.value == 'new') router.push({ path: `/${store.userStore.cliente}/${store.userStore.dominio}/pv-tecnico/${itemData.value.id}` });
+                    mode.value = 'view';
+                } else {
+                    defaultWarn('Erro ao salvar registro');
+                }
+            })
+            .catch((err) => {
+                defaultWarn(err.response.data);
+            });
+    }
+};
+// Verifica se houve alteração nos dados do formulário
+const isItemDataChanged = () => {
+    const ret = JSON.stringify(itemData.value) !== JSON.stringify(itemDataComparision.value);
+    if (!ret) {
+        accept.value = false;
+        // errorMessages.value = {};
+    }
+    return ret;
+};
+// Validar formulário
+const formIsValid = () => {
+    return true;
+};
+// Recarregar dados do formulário
+const reload = () => {
+    mode.value = 'view';
+    accept.value = false;
+    errorMessages.value = {};
+    loadData();
+    emit('cancel');
+};
+// Carregar dados do formulário
+onBeforeMount(() => {
+    loadData();
+    // loadOptions();
+});
+onMounted(() => {
+    if (props.mode && props.mode != mode.value) mode.value = props.mode;
+    else {
+        if (itemData.value.id) mode.value = 'view';
+        else mode.value = 'new';
+    }
+});
+// Observar alterações nos dados do formulário
+watchEffect(() => {
+    isItemDataChanged();
+});
+const menu = ref();
+const preview = ref(false);
+const items = ref([
+    {
+        label: 'View',
+        icon: 'pi pi-fw pi-search',
+        command: () => {
+            alert('Enviar nova imagem');
+        }
+    },
+    {
+        label: 'Delete',
+        icon: 'pi pi-fw pi-trash',
+        command: () => {
+            alert('Excluir imagem');
+        }
+    }
+]);
+</script>
+
+<template>
+    <Breadcrumb v-if="mode != 'new'" :items="[{ label: 'Todos os Parâmetros', to: `/${userData.cliente}/${userData.dominio}/pv-tecnicos` }, { label: itemData.registro + (store.userStore.admin >= 1 ? `: (${itemData.id})` : '') }]" />
+    <div class="card">
+        <form @submit.prevent="saveData">
+            <div class="grid">
+                <div class="col-12">
+                    <div class="p-fluid grid">
+                        <div class="col-12 md:col-4">
+                            <label for="tecnico">{{ labels.tecnico }}</label>
+                            <Skeleton v-if="loading.form" height="3rem"></Skeleton>
+                            <InputText v-else autocomplete="no" :disabled="mode == 'view'" v-model="itemData.tecnico" id="tecnico" type="text" />
+                        </div>
+                        <div class="col-12 md:col-4">
+                            <label for="telefone_contato">{{ labels.telefone_contato }}</label>
+                            <Skeleton v-if="loading.form" height="3rem"></Skeleton>
+                            <InputText v-else autocomplete="no" :disabled="mode == 'view'" v-maska data-maska="['(##) ####-####', '(##) #####-####']" v-model="itemData.telefone_contato" id="telefone_contato" type="text" />
+                        </div>
+                        <div class="col-12 md:col-4">
+                            <label for="email_contato">{{ labels.email_contato }}</label>
+                            <Skeleton v-if="loading.form" height="3rem"></Skeleton>
+                            <InputText v-else autocomplete="no" :disabled="mode == 'view'" v-model="itemData.email_contato" id="tecnico" type="text" />
+                        </div>
+                        <div class="col-12 md:col-12">
+                            <label for="observacao">{{ labels.observacao }}</label>
+                            <Skeleton v-if="loading.form" height="3rem"></Skeleton>
+                            <InputText v-else autocomplete="no" :disabled="mode == 'view'" v-model="itemData.observacao" id="observacao" type="text" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="card flex justify-content-center flex-wrap gap-3">
+                        <Button type="button" v-if="mode == 'view'" label="Editar" icon="fa-regular fa-pen-to-square fa-beat" text raised @click="mode = 'edit'" />
+                        <Button type="submit" v-if="mode != 'view'" label="Salvar" icon="pi pi-save" severity="success" text raised :disabled="!isItemDataChanged() || !formIsValid()" />
+                        <Button type="button" v-if="mode != 'view'" label="Cancelar" icon="pi pi-ban" severity="danger" text raised @click="reload" />
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
