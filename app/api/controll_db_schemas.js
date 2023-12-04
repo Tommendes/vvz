996c808ae9492183625f6831b5cd3f802c781a9e
Tom Mendes <contato@tommendes.com.br>
2023-12-04 16:53:20 -0300

    Api
    Testando criação dinâmica de esquemas

--- app/api/controll_db_schemas.js
+++ app/api/controll_db_schemas.js
@@ -1,9 +1,17 @@
-const { dbPrefix } = require("../.env")
+const { dbPrefix, db } = require("../.env")
 module.exports = app => {
     const { existsOrError, notExistsOrError, cpfOrError, cnpjOrError, lengthOrError, emailOrError, isMatchOrError, noAccessMsg } = app.api.validation
-    const tabelaSchemas = 'schemas_control'    
+    const tabelaSchemas = 'schemas_control'
     const crypto = require("crypto");
-    
+    const mysql = require('mysql');
+
+    const dbConfig = {
+        host: db.host,
+        user: 'vivazul_ger3nci1d0r',
+        password: 's3nh@ 1t1lizad@ pa4a gerenciar',
+        database: db.database,
+    };
+
     const creatClientSchema = async (req, res) => {
         let user = req.user
         const uParams = await app.db('users').where({ id: user.id }).first();
@@ -16,22 +24,64 @@ module.exports = app => {
 
         const body = { ...req.body }
 
-        const schemaName = body.schema_name || 'vivazul_' + crypto.randomBytes(3).toString('hex');
-        const userName = schemaName;
-        const publicIp = req.socket.remoteAddress || req.connection.remoteAddress || req.headers['x-forwarded-for'] || req.connection.socket.remoteAddress;
-        const userHost = body.userHost || publicIp || '162.214.208.90';
-        console.log('userHost: ', userHost);
+        // Conecta ao banco de dados
+        const connection = mysql.createConnection(dbConfig);
+        connection.connect();
+
+        const schemaNameAndUser = body.schema_name || dbPrefix + '_' + crypto.randomBytes(3).toString('hex');
+        dbConfig.publicIp = req.socket.remoteAddress || req.connection.remoteAddress || req.headers['x-forwarded-for'] || req.connection.socket.remoteAddress;
+        dbConfig.publicIp = body.userHost || dbConfig.publicIp || '162.214.208.90';
+        console.log('dbConfig: ', dbConfig);
 
         try {
-            await app.db.raw(`CREATE DATABASE ${schemaName};`)
-            await app.db.raw(`CREATE USER '${userName}'@'${userHost}';`)
-            await app.db.raw(`GRANT ALTER, CREATE, CREATE TEMPORARY TABLES, DELETE, EXECUTE, INSERT, LOCK TABLES, REFERENCES, SELECT, SHOW VIEW, UPDATE ON \`${schemaName}\`.* TO '${userName}'@'${userHost}';`)
-            return res.send(`Schema and user created with name ${schemaName}`);
+            // Crie um banco de dados
+            await queryAsync(connection, `CREATE DATABASE ${schemaNameAndUser};`);
+            // Conecte-se ao novo banco de dados
+            await queryAsync(connection, `USE ${schemaNameAndUser};`);
+            // Crie um novo usuário para o banco de dados
+            await queryAsync(connection, `CREATE USER '${schemaNameAndUser}'@'${dbConfig.publicIp}' IDENTIFIED BY '${dbConfig.password}';`);
+            // Conceda privilégios CRUD ao usuário no banco de dados
+            await queryAsync(connection, `GRANT SELECT, INSERT, UPDATE, DELETE ON ${schemaNameAndUser}.* TO '${schemaNameAndUser}'@'${dbConfig.publicIp}';`);
+            await queryAsync(connection, `FLUSH PRIVILEGES;`);
+
+            return res.send(`Schema and user created with name ${schemaNameAndUser}`);
         } catch (error) {
-            app.api.logger.logError({ log: { line: `Error in file: ${__filename} (${__function}:${__line}). Error: ${error}`, sConsole: true } })
+            app.api.logger.logError({ log: { line: `Erro ao criar esquema: ${__filename} (${__function}:${__line}). Error: ${error}`, sConsole: true } })
             return res.status(500).send(error)
+        } finally {
+            // Fecha a conexão
+            connection.end();
         }
+
+
+        // app.db.raw(`CREATE DATABASE ${schemaUserName};`)
+        //     .then(() => {
+        //         app.db.raw(`CREATE USER '${schemaUserName}'@'${dbConfig.publicIp}';`)
+        //             .then(() => {
+        //                 app.db.raw(`GRANT ALTER, CREATE, CREATE TEMPORARY TABLES, DELETE, EXECUTE, INSERT, LOCK TABLES, REFERENCES, SELECT, SHOW VIEW, UPDATE ON \`${schemaUserName}\`.* TO '${schemaUserName}'@'${dbConfig.publicIp}';`)
+        //             })
+        //             .catch((error) => {
+        //                 return res.status(500).send(error)
+        //             })
+        //         return res.send(`Schema and user created with name ${schemaUserName}`);
+        //     })
+        //     .catch((error) => {
+        //         app.api.logger.logError({ log: { line: `Error in file: ${__filename} (${__function}:${__line}). Error: ${error}`, sConsole: true } })
+        //         return res.status(500).send(error)
+        //     })
     }
+
+    function queryAsync(connection, sql) {
+        return new Promise((resolve, reject) => {
+          connection.query(sql, (error, results) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(results);
+            }
+          });
+        });
+      }
 
     // const setNewSchemaOnDB = async (req, res) => {
     //     const schemaName = req.params.schemaName;
