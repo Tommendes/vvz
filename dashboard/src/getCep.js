3a8721e20cfbc623d9f3fa4b845909941584d100
Tom Mendes <contato@tommendes.com.br>
2024-01-23 17:30:53 -0300

    Dashboard
    Adicionado o método para recuperar endereço com api viacep

--- dashboard/src/getCep.js
+++ dashboard/src/getCep.js
@@ -1,4 +1,11 @@
 import axios from 'axios';
+axios.interceptors.request.use((config) => {
+    delete config.headers['x-ip-address'];
+    delete config.headers['x-geo-lt'];
+    delete config.headers['x-geo-ln'];
+    setTimeout(() => {}, 200);
+    return config;
+});
 export const getViaCep = async (cep) => {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
     await axios.get(url).then((res) => {
