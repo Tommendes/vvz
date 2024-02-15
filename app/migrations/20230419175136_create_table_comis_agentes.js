a0eff824abef3d39c90aa6b1efdfc8c2a49c9158
Tom Mendes <contato@tommendes.com.br>
2024-02-15 08:17:38 -0300

    API

--- app/migrations/20230419175136_create_table_comis_agentes.js
+++ app/migrations/20230419175136_create_table_comis_agentes.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('vivazul_bceaa5.com_agentes', table => {
+    return knex.schema.createTable('vivazul_bceaa5.comis_agentes', table => {
         table.engine('InnoDB')
         table.charset('utf8mb4')
         table.collate('utf8mb4_general_ci')
@@ -9,7 +9,6 @@ exports.up = function(knex, Promise) {
         table.string('updated_at')
         table.integer('status').defaultTo(0).notNull().comment('Status do registro (INATIVO:0; ATIVO:10; EXCLUÍDO:99)')
         table.string('ordem',3).comment('Número identificador próprio')
-        table.integer('id_users').unsigned().references('id').inTable('vivazul_api.users').onUpdate('Cascade').onDelete('NO ACTION').comment('Registro no cadastro')
         table.integer('id_cadastros').unsigned().notNull().references('id').inTable('cadastros').onUpdate('Cascade').onDelete('NO ACTION').comment('Registro no cadastro')
         table.tinyint('dsr',1).notNull().defaultTo(0).comment('Recebe DSR')
         table.string('observacao').comment('Observação do registro')
@@ -17,5 +16,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('vivazul_bceaa5.com_agentes')
+    return knex.schema.dropTable('vivazul_bceaa5.comis_agentes')
 };
