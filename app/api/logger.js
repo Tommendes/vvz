1fb2ce933941fa91e91a2841a6f2bd1fc8263208
Tom Mendes <contato@tommendes.com.br>
2024-06-05 17:37:46 -0300

    API
    Logs com fuso GMT-3

--- app/api/logger.js
+++ app/api/logger.js
@@ -1,19 +1,26 @@
 const fs = require('fs')
+const moment = require('moment');
 
 module.exports = app => {
     const logInfo = async (req, res) => {
         const log = { ...req.log }
-        const stream = fs.createWriteStream("./logs/info.txt", { flags: 'a' });
-        const line = `[${new Date().toISOString()}] ${req.user ? req.user.name : ''}: ${log.line}`
+        stream = fs.createWriteStream("./logs/info.txt", { flags: 'a' });
+
+        // Usar moment para obter a data no fuso horário GMT-3
+        const timestamp = moment().utcOffset('-03:00').format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");    
+        const line = `[${timestamp}] ${req.user ? req.user.name : ''}: ${log.line}`;
         stream.write(line + "\n");
         stream.end();
         if (log.sConsole) return console.log(line);
     }
-    
+
     const logError = async (req, res) => {
         const log = { ...req.log }
-        const stream = fs.createWriteStream("./logs/errors.txt", { flags: 'a' });
-        const line = `[${new Date().toISOString()}] ${req.user ? req.user.name : ''}: ${log.line}`
+        stream = fs.createWriteStream("./logs/errors.txt", { flags: 'a' });
+
+        // Usar moment para obter a data no fuso horário GMT-3
+        const timestamp = moment().utcOffset('-03:00').format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");    
+        const line = `[${timestamp}] ${req.user ? req.user.name : ''}: ${log.line}`;
         stream.write(line + "\n");
         stream.end();
         if (log.sConsole) return console.log(line);
