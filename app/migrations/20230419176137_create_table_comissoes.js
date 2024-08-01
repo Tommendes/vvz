ec5e472a601c92f1b0dd427d96f8eb4a85af9d59
Tom Mendes <contato@tommendes.com.br>
2024-08-01 14:20:57 -0300

    API e Dashboard
    Melhorias de desempenho em PipelineGrid e API migrations melhoradas e referendando db.database e migrationClientSchema em .env

--- app/migrations/20230419176137_create_table_comissoes.js
+++ app/migrations/20230419176137_create_table_comissoes.js
@@ -10,7 +10,7 @@ exports.up = function (knex, Promise) {
         table.string('created_at').notNull()
         table.string('updated_at')
         table.integer('status').defaultTo(0).notNull().comment('Status do registro (INATIVO:0; ATIVO:10; EXCLUÍDO:99)')
-        table.integer('id_pipeline', 10).unsigned().notNull().comment('Pipeline relacionado ')
+        table.integer('id_pipeline', 10).unsigned().notNull().comment('Pipeline relacionado ').references('id').inTable(migrationClientSchema + '.pipeline').onUpdate('Cascade').onDelete('NO ACTION');
         table.integer('id_comis_agentes', 10).unsigned().notNull().comment('Agente').references('id').inTable(migrationClientSchema + '.comis_agentes').onUpdate('Cascade').onDelete('NO ACTION')
         table.decimal('valor_base', 10, 2).comment('Valor base de cálculo da comissão')
         table.decimal('desconto', 10, 2).comment('Percentual sobre a base')
