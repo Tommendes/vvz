3df2e03a8f169a7cd4c3e0895f6dcf8fcef3e5fc
Tom Mendes <contato@tommendes.com.br>
2024-08-01 14:19:31 -0300

    API e Dashboard
    Melhorias de desempenho em PipelineGrid e API migrations melhoradas e referendando db.database e migrationClientSchema em .env

--- app/migrations/20230419182123_alter_table_comissoes_pipeline.js
+++ app/migrations/20230419182123_alter_table_comissoes_pipeline.js
@@ -6,13 +6,13 @@ const { migrationClientSchema } = require('../.env')
  */
 exports.up = function(knex) {
     return knex.schema.alterTable(migrationClientSchema + '.comissoes', table => {
-        table.dropForeign('id_pipeline');
-        table.foreign('id_pipeline').references('id').inTable(migrationClientSchema + '.pipeline').onUpdate('Cascade').onDelete('NO ACTION');
+        table.dropForeign(knex.raw('vivazul_bceaa5_comissoes_id_pipeline_foreign'));
+        table.foreign(knex.raw('vivazul_comissoes_id_pipeline_foreign.id_pipeline')).references('id').inTable(migrationClientSchema + '.pipeline').onUpdate('Cascade').onDelete('NO ACTION');
     })
 };
 
 exports.down = function (knex, Promise) {
     return knex.schema.alterTable(migrationClientSchema + '.comissoes', table => {
-        table.dropForeign('id_pipeline');
+        table.dropForeign(knex.raw('vivazul_comissoes_id_pipeline_foreign.id_pipeline'));
     })
 };
