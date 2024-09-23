ac5bf1fc12b88552e0b16844b43ae4e1d9bca7f0
Tom Mendes <contato@tommendes.com.br>
2024-09-23 17:46:48 -0300

    API e Dashboard Mais aprimoramentos no sistema de mensagens do WhatsApp Correçõs em FinanceiroGrid

---
+++ app/migrations/20240923132500_create_table_whats_msgs.js
@@ -0,0 +1,30 @@
+const { migrationClientSchema } = require('../.env')
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable(migrationClientSchema + '.whats_msgs', table => {
+        table.engine('InnoDB')
+        table.charset('utf8mb4')
+        table.collate('utf8mb4_general_ci')
+        table.increments('id').primary()
+        table.integer('evento').notNull()
+        table.string('created_at').notNull()
+        table.string('updated_at')
+        table.integer('status').defaultTo(10).notNull().comment('Status do registro (INATIVO:0; ATIVO:10; EXCLUÍDO:99)')
+        table.string('schedule').notNull().comment('Data e hora do agendamento do envio')
+        table.string('phone').notNull().comment('Número do telefone')
+        table.string('message', 500).notNull().comment('Mensagem a ser enviada')
+        table.integer('situacao').defaultTo(1).notNull().comment('Situação da parcela (1: Aberto; 2: Enviada; 99: Cancelado)')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('vivazul_bceaa5.whats_msgs')
+};
