f28d170b1c4bdab9affc09a46850e7e407a262fd
Tom Mendes <contato@tommendes.com.br>
2024-10-09 17:16:51 -0300

    API
    Novas operações em whats_msgs

--- app/api/whats_msgs.js
+++ app/api/whats_msgs.js
@@ -90,7 +90,7 @@ module.exports = app => {
                         recurrence.msg_id = msgId;
                         await app.db(`${dbPrefix}_${uParams.schema_name}.whats_msgs_recurrences`).insert(recurrence);
                     }
-                    if (body.schedule <= moment().format('YYYY-MM-DD HH:mm:ss')) await sendMessage({ id: msgId, message: body.message, id_profile: body.id_profile, phone: body.phone }, uParams, tabelaDomain)
+                    if (body.schedule <= moment().format('YYYY-MM-DD HH:mm:ss')) await sendMessage({ id: msgId, identified: body.identified, message: body.message, id_profile: body.id_profile, phone: body.phone }, uParams, tabelaDomain)
                     return res.status(200).send({ id: msgId })
                 })
                 .catch(error => {
