8361cdb1bf531800d4c8a2034dfba48b19f49997
Tom Mendes <contato@tommendes.com.br>
2024-10-29 11:45:15 -0300

    API
    Correção na informação da data de pagamento

--- app/api/fin_parcelas.js
+++ app/api/fin_parcelas.js
@@ -52,13 +52,16 @@ module.exports = app => {
             // Verificar se a data de vencimanto é válida e converte para en
             if (!moment(body.data_vencimento, 'DD/MM/YYYY', true).isValid()) throw 'Data de vencimanto inválida'
             body.data_vencimento = moment(body.data_vencimento, 'DD/MM/YYYY').format('YYYY-MM-DD')
-            if (body.data_pagto && !moment(body.data_pagto, 'DD/MM/YYYY', true).isValid()) {
-                body.data_pagto = null
-                throw 'Data de pagamento inválida' 
-            }
-            console.log('body.data_pagto', body.data_pagto);
-            
-            body.data_pagto = moment(body.data_pagto, 'DD/MM/YYYY').format('YYYY-MM-DD') || null
+            if (body.situacao == SITUACAO_ABERTO) body.data_pagto = null
+            else if ([SITUACAO_CONCILIADO, SITUACAO_PAGO].includes(body.situacao)) {
+                if ((body.data_pagto || moment(body.data_pagto, 'DD/MM/YYYY', true).isValid())) {
+                    body.data_pagto = null
+                    throw 'Data de pagamento inválida'
+                }
+            } else {
+                body.data_pagto = moment(body.data_pagto, 'DD/MM/YYYY').format('YYYY-MM-DD')
+            }            
+
             if (['2', '3'].includes(String(body.situacao))) {
                 existsOrError(body.data_pagto, 'Data de pagamento não informada')
                 existsOrError(body.id_fin_contas, `Conta de ${centroCusto == '1' ? 'recebimento' : 'pagamento ou conciliação'} não informada`)
