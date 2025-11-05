4bbb2db758d949f3f64e6663d63896631d845a5c
Tom Mendes <contato@tommendes.com.br>
2025-11-05 15:36:10 -0300

    Novo no VPS

--- app/api/mailer.js
+++ app/api/mailer.js
@@ -1,10 +1,12 @@
 const nodemailer = require("nodemailer")
-const { mailer, mailerBot } = require('../.env')
+const { mailer } = require('../.env')
+// const { mailer, mailerBot } = require('../.env')
 
 module.exports = app => {
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport(mailer);
-    const transporterBot = nodemailer.createTransport(mailerBot);
+    // const transporterBot = nodemailer.createTransport(mailerBot);
 
-    return { transporter, transporterBot }
+    return { transporter }
+    // return { transporter, transporterBot }
 }
